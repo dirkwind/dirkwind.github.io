e2930ba6d9b16e21193b633f5cd7a7ec2cbbedb4
dirkwind <dirkwind707@gmail.com>
2023-03-07 20:56:19 -0600

    add documentation comments, update page title

--- gravity/scale.js
+++ gravity/scale.js
@@ -1,3 +1,6 @@
+/**
+ * Keeps track of a ratio between pixels and meters
+ */
 class Scale {
 
     /**
@@ -5,17 +8,47 @@ class Scale {
      * @param {number} meters 
      * @param {number} pixels 
      */
-    constructor(meters = 1000, pixels = 1) {
-        this.meters = meters;
-        this.pixels = pixels;
+    constructor(meters = 1, pixels = 1) {
+        this._meters = meters;
+        this._pixels = pixels;
     }
 
+    get meters() {
+        return self._meters;
+    }
+
+    set meters(newVal) {
+        if (!(newVal instanceof Number) || newVal < 0)
+            throw new Error("Meters must be a positive number.");
+
+        self._meters = newVal;
+    }
+
+    get pixels() {
+        return self._pixels;
+    }
+
+    set pixels(newVal) {
+        if (!(newVal instanceof Number) || newVal < 0)
+            throw new Error("Meters must be a positive number.");
+
+        self._pixels = newVal;
+    }
+
+    /**
+     * 
+     * @returns {number} the ratio between meters and pixels
+     */
     metersPerPixel() {
-        return this.meters/this.pixels;
+        return this._meters/this._pixels;
     }
 
+    /**
+     * 
+     * @returns {number} the ratio between pixels and meters
+     */
     pixelsPerMeter() {
-        return this.pixels/this.meters;
+        return this._pixels/this._meters;
     }
 
 }
